7b594eae5bd73d53d9b624a4690cba7ba8d6fb1e
TimurFromCanada <timurfromcanada@gmail.com>
2022-06-09 21:01:13 +0500

    Project is ready

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StructuresPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                var stroka = Console.ReadLine();
+                var number = int.Parse(Console.ReadLine());
+
+                if (number < 0 || number > 6)
+                {
+                    Console.WriteLine("Цвета с таким номером не существует");
+                    continue;
+                }
+                    
+                StaticPrint.Print(stroka, number);
+            }
+        }
+    }
+}
---
+++ StaticPrint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace StructuresPrint
+{
+    public enum ConsoleColor
+    {
+        Black = 0,
+        Blue = 1,
+        Red = 2,
+        Yellow = 3,
+        Gray = 4,
+        Green = 5,
+        White = 6,
+    }
+
+    public class StaticPrint
+    {
+        public static void Print(string stroka, int color)
+        {
+            Console.ForegroundColor = (System.ConsoleColor)color;
+            Console.WriteLine(stroka);
+            Console.ResetColor();
+        }
+    }
+}
